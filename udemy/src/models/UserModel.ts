85d0d44346b88cd33501d034d659e97838a8ccb8
Leonardo Fontes <leofontes@gmail.com>
2018-02-21 15:23:57 -0300

    Comment and Post model

--- udemy/src/models/UserModel.ts
+++ udemy/src/models/UserModel.ts
@@ -1,5 +1,7 @@
 import * as Sequelize from 'sequelize';
-import { BaseModelInterface } from '../interfaces/BaseModelInterface'
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
+import { BaseModelInterface } from '../interfaces/BaseModelInterface';
+
 
 export interface UserAttributes {
   id?: number;
@@ -7,6 +9,8 @@ export interface UserAttributes {
   email?: string;
   password?: string;
   photo?: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface UserInstance extends Sequelize.Instance<UserAttributes>, UserAttributes {
@@ -48,7 +52,19 @@ export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes):
       allowNull: true,
       defaultValue: null
     }
-  })
+  }, {
+    tableName: 'users',
+    hooks: {
+      beforeCreate: (user: UserInstance, options: Sequelize.CreateOptions): void => {
+        const salt = genSaltSync();
+        user.password = hashSync(user.password, salt);
+      }
+    }
+  });
+
+  User.prototype.isPassword = (encodedPassword: string, password: string) => {
+    return compareSync(password, encodedPassword);
+  }
 
   return User;
 }
